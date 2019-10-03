1afdd66de1adb1300c14821f03b47ba4f85918fe
Gavin Watson <gavset99@gmail.com>
2019-10-03 17:39:35 -0400

    Initial Commit

---
+++ src/main/java/com/mandynotes/MandynotesApplication.java
@@ -0,0 +1,13 @@
+package com.mandynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandynotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandynotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mandynotes/MandynotesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mandynotes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MandynotesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
